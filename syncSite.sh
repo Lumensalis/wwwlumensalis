209e9e2e9c5913114ac47198e8616a0ef0249b3e
James Fowler <james@openseaconsulting.com>
2025-06-13 17:36:17 -0400

    setup rsync updates

--- syncSite.sh
+++ syncSite.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rsync -avz -e "ssh -p7822 -i /mnt/c/Users/jbrel/.ssh/id_rsa_lumensalis" \
+rsync -avz -e "ssh -p7822 -i ~/.ssh/id_rsa_lumensalis" \
     out/*    \
     whackybo@whackyboo.com:public_html/lumensalis.com
