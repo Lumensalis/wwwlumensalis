ca5b1e05dffbae7d8eaf685afd46cbffe7438e39
James Fowler <james@openseaconsulting.com>
2025-06-15 11:27:20 -0400

    added MDXTest docs

--- src/content/Topics/SoftwareDevelopment/_meta.ts
+++ src/content/Topics/SoftwareDevelopment/_meta.ts
@@ -3,8 +3,8 @@ import type { MetaRecord } from 'nextra';
 const meta : MetaRecord = {
 
     'Conceit' : {display:'hidden'},
-
-
+    //'MDXTest' : {display:'hidden'},
+    'MDXTest2' : {display:'hidden'}
 };
 
 export default meta;
